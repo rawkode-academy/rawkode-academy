227e8f195aee99c058acd5605af67c79bd219839
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2025-12-16 11:32:09 +0100

    chore: nuke the studio for now
    - keep DNS

--- projects/rawkode.studio/src/app/providers/index.ts
+++
@@ -1,2 +0,0 @@
-export { AuthProvider, useAuth } from "./AuthProvider";
-export { ThemeProvider, useTheme } from "./ThemeProvider";
--- projects/rawkode.studio/src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-// Radix UI Primitives with Tailwind styling
-export * from "./button";
-export * from "./dialog";
-export * from "./dropdown-menu";
-export * from "./theme-toggle";
