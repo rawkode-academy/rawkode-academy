b5a3fc9995e463850697ee30baa9930caff34dfb
David Flanagan <david@rawkode.dev>
2026-04-02 10:13:09 +0100

    fix(rawkode.academy/website): fix build errors

--- projects/rawkode.academy/website/src/types/global.d.ts
+++ projects/rawkode.academy/website/src/types/global.d.ts
@@ -4,6 +4,7 @@ interface Window {
 		identify: (distinctId: string, properties?: Record<string, unknown>) => void;
 		register: (properties: Record<string, unknown>) => void;
 	};
+	enablePostHog?: () => void;
 	grafanaFaro?: {
 		api: {
 			pushEvent: (event: string, attributes?: Record<string, unknown>) => void;
@@ -11,4 +12,5 @@ interface Window {
 			setSession: (session: { attributes?: Record<string, unknown> }) => void;
 		};
 	} | null;
+	enableGrafanaFaro?: () => void;
 }
